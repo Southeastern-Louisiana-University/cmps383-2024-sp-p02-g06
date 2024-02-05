39014b1464875ff101d6b7deaed34942dc49c002
Sheylo1 <wesley.austin-2@selu.edu>
2024-02-05 13:37:28 -0600

    Added Entities
    Entities that create a many-to-many relationship between tables.

---
+++ Selu383.SP24.Api/Features/Roles/Role.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Hosting;
+
+namespace Selu383.SP24.Api.Features
+{
+    public class Role : IdentityRole<int>
+    {
+        public int Id { get; set; }
+
+        public virtual ICollection<UserRole> Users { get; } = new List<UserRole>();
+    }
+}
---
+++ Selu383.SP24.Api/Features/UserRoles/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Selu383.SP24.Api.Features
+{
+    public class UserRole : IdentityUserRole<int>
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+
+        public User User { get; set; }
+        public Role Role { get; set; }
+    }
+}
