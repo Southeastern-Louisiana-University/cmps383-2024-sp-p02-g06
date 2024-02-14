aae4b0e1ef9220b0b66a96737c23309006faaa60
TylerEscuriex <tyler.escuriex@selu.edu>
2024-02-13 21:56:39 -0600

    Revert "- made required types nullable"
    This reverts commit b24ebf45787477f4106c6525343ea70c90233826.

--- Selu383.SP24.Api/Features/Users/UserCreateDto.cs
+++ Selu383.SP24.Api/Features/Users/UserCreateDto.cs
@@ -4,7 +4,7 @@ namespace Selu383.SP24.Api.Features;
 
 public class UserCreateDto
 {
-    public string? UserName { get; set; }
-    public string[]? Roles { get; set; }
-    public string? Password { get; set; }
+    public string UserName { get; set; }
+    public string[] Roles { get; set; }
+    public string Password { get; set; }
 }
